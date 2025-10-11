0d7b378abfddcd0b38b983b6708a23c975095b1c
NoahTesson <noah.tesson@epitech.eu>
2025-10-11 20:15:52 +0200

    [ADD] - new class repositories for each table to handle their data and upd class database to create temporary databse for tests

---
+++ geneweb-python/geneweb/core/repositories/person_repository.py
@@ -0,0 +1,35 @@
+from sqlalchemy.orm import Session
+from geneweb.core.models.Person import Person
+
+class PersonRepository:
+    def __init__(self, session: Session):
+        self.session = session
+
+    def add_person(self, first_name: str, last_name: str, gender: str = None,
+                   birth_date=None, death_date=None, birth_place=None,
+                   death_place=None, occupation=None, notes=None):
+        person = Person(
+            first_name=first_name,
+            last_name=last_name,
+            gender=gender,
+            birth_date=birth_date,
+            death_date=death_date,
+            birth_place=birth_place,
+            death_place=death_place,
+            occupation=occupation,
+            notes=notes
+        )
+        self.session.add(person)
+        self.session.commit()
+        return person
+    
+    def get_a_person_by_first_name(self, person_first_name: str):
+        return self.session.query(Person).filter(Person.first_name == person_first_name).first()
+    
+    def delete_person_by_first_name(self, first_name: str):
+        person = self.get_a_person_by_first_name(first_name)
+        if person:
+            self.session.delete(person)
+            self.session.commit()
+            return True
+        return False
