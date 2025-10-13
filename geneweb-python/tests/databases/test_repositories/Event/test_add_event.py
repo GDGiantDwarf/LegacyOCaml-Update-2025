e62b037218e09493a1a6f464b7092a547b158396
NoahTesson <noah.tesson@epitech.eu>
2025-10-13 17:25:54 +0200

    [ADD] - event repo and tests

---
+++ geneweb-python/tests/databases/test_repositories/Event/test_add_event.py
@@ -0,0 +1,9 @@
+from conftest import db
+from datetime import date
+from geneweb.core.repositories.event_repository import EventRepository
+
+def test_add_one_event(db):
+    repo = EventRepository(db.session)
+
+    event = repo.add_event(event_type="marriage")
+    assert event is not None
---
+++ geneweb-python/tests/databases/test_repositories/Event/test_get_event.py
@@ -0,0 +1,25 @@
+from conftest import db
+from datetime import date
+from geneweb.core.repositories.event_repository import EventRepository
+
+def test_add_one_event_and_get_it(db):
+    repo = EventRepository(db.session)
+    event_type = "marriage"
+
+    event = repo.add_event(event_type=event_type)
+    assert event is not None
+
+    events = repo.get_event_by_id(event.id)
+    assert events is not None
+    assert events.event_type == event_type
+
+def test_add_one_event_and_get_a_wrong_one(db):
+    repo = EventRepository(db.session)
+    event_type = "marriage"
+
+    event = repo.add_event(event_type=event_type)
+    assert event is not None
+
+    events = repo.get_event_by_id(5)
+    print(events)
+    assert events is None
