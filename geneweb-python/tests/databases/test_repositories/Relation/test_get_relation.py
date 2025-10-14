566085f62864992a87a6d9184020d3aedc4417ea
NoahTesson <noah.tesson@epitech.eu>
2025-10-14 12:01:45 +0200

    [ADD] - Relation repo for CRUD and tests

---
+++ geneweb-python/tests/databases/test_repositories/Relation/test_get_relation.py
@@ -0,0 +1,39 @@
+import pytest
+from conftest import db
+from geneweb.core.repositories.relation_repository import RelationRepository
+from geneweb.core.repositories.person_repository import PersonRepository
+
+def test_get_a_relation(db):
+    repo_person = PersonRepository(db.session)
+    repo = RelationRepository(db.session)
+
+    person1 = repo_person.add_person("Noe", "gallam")
+    person2 = repo_person.add_person("luci", "dubois")
+
+    relation = repo.add_relation(person1.id, person2.id, "amant")
+    assert relation is not None
+    assert relation.person1_id == person1.id
+    assert relation.person2_id == person2.id
+
+
+    relation_get = repo.get_relation_by_id(relation.id)
+    assert relation_get is not None
+    assert relation_get.person1_id == relation.person1_id
+    assert relation_get.person2_id == relation.person2_id
+    assert relation_get.relation_type == relation.relation_type
+
+
+def test_get_a_wrong_relation(db):
+    repo_person = PersonRepository(db.session)
+    repo = RelationRepository(db.session)
+
+    person1 = repo_person.add_person("Noe", "gallam")
+    person2 = repo_person.add_person("luci", "dubois")
+
+    relation = repo.add_relation(person1.id, person2.id, "amant")
+    assert relation is not None
+    assert relation.person1_id == person1.id
+    assert relation.person2_id == person2.id
+
+    relation_get = repo.get_relation_by_id(4)
+    assert relation_get is None
---
+++ geneweb-python/tests/databases/test_repositories/Relation/test_update_relation.py
@@ -0,0 +1,42 @@
+import pytest
+from conftest import db
+from geneweb.core.repositories.relation_repository import RelationRepository
+from geneweb.core.repositories.person_repository import PersonRepository
+
+def test_update_relation(db):
+    repo_person = PersonRepository(db.session)
+    repo = RelationRepository(db.session)
+
+    person1 = repo_person.add_person("Noe", "gallam")
+    person2 = repo_person.add_person("luci", "dubois")
+
+    relation = repo.add_relation(person1.id, person2.id, "amant")
+    assert relation is not None
+    assert relation.person1_id == person1.id
+    assert relation.person2_id == person2.id
+    assert relation.relation_type == "amant"
+
+    relation_update = repo.update_relation_by_id(relation.id, relation_type="friend")
+    assert relation_update is not None
+    assert relation_update.person1_id == person1.id
+    assert relation_update.person2_id == person2.id
+    assert relation_update.relation_type == "friend"
+
+
+def test_update_relation_with_wrong_relation_id(db):
+    repo_person = PersonRepository(db.session)
+    repo = RelationRepository(db.session)
+
+    person1 = repo_person.add_person("Noe", "gallam")
+    person2 = repo_person.add_person("luci", "dubois")
+
+    relation = repo.add_relation(person1.id, person2.id, "amant")
+    assert relation is not None
+    assert relation.person1_id == person1.id
+    assert relation.person2_id == person2.id
+    assert relation.relation_type == "amant"
+
+    with pytest.raises(ValueError) as ecxinfo:
+        repo.update_relation_by_id(5, relation_type="friend")
+
+    assert "relation_id Invalid" in str(ecxinfo.value)
