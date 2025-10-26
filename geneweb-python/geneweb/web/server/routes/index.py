7f9053c2846369a12ceada0780a8dfcfa1cc12df
NoahTesson <noah.tesson@epitech.eu>
2025-10-26 16:10:50 +0100

    [ADD] - list available bases in index route

--- geneweb-python/geneweb/web/server/routes/index.py
+++ geneweb-python/geneweb/web/server/routes/index.py
@@ -9,18 +9,23 @@ router = APIRouter()
 
 @router.get("/", response_class=HTMLResponse)
 async def index(request: Request):
+    bases = Database.get_existing_bases()
     templates = request.app.state.templates
-    return templates.TemplateResponse(request, "index.html", templates.get_context(request))
+    context = templates.get_context(request)
+    context.update({
+        "list_bases": bases
+    })
+    return templates.TemplateResponse(request, "index.html", context)
 
-@router.post("/select-base")
-async def select_base(request: Request, base_name: str = Form(...)):
-    # Ici tu peux vérifier si la base existe, etc.
+@router.post("/select-base/{base_name}")
+async def select_base(request: Request, base_name: str):
     if not Database.is_base_exist(base_name):
         templates = request.app.state.templates
-        context = templates.get_context(request)
+        context = templates.get_context()
         context.update({
+            "request": request,
             "error_message": f"La base '{base_name}' n'existe pas"
         })
-        return templates.TemplateResponse("index.html", context)
-    url = f"/base/{base_name}"  # URL vers la page de gestion
-    return RedirectResponse(url=url, status_code=303)  # PRG pattern
+        return templates.TemplateResponse(request, "index.html", context)
+    
+    return RedirectResponse(url=f"/base/{base_name}", status_code=303)
